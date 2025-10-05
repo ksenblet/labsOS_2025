bac11f76b96a4428d8b84ebeb8ef2c76db7f3943
ksenblet <ksenia4lev@gmail.com>
2025-10-06 00:56:27 +0300

    lab 1

---
+++ lab1OS/build/CMakeFiles/child1.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/kseni/Downloads/Telegram Desktop/lab1OSLinuxToWind/lab1OS/src/child1.cpp" "CMakeFiles/child1.dir/src/child1.cpp.obj" "gcc" "CMakeFiles/child1.dir/src/child1.cpp.obj.d"
+  "C:/Users/kseni/Downloads/Telegram Desktop/lab1OSLinuxToWind/lab1OS/src/systemCall.cpp" "CMakeFiles/child1.dir/src/systemCall.cpp.obj" "gcc" "CMakeFiles/child1.dir/src/systemCall.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lab1OS/build/CMakeFiles/parent.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/parent.dir/src/parent.cpp.obj"
+  "CMakeFiles/parent.dir/src/parent.cpp.obj.d"
+  "CMakeFiles/parent.dir/src/systemCall.cpp.obj"
+  "CMakeFiles/parent.dir/src/systemCall.cpp.obj.d"
+  "bin/parent.exe"
+  "bin/parent.exe.manifest"
+  "bin/parent.pdb"
+  "libparent.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/parent.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
